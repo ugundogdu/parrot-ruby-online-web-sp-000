17585bb5334155e45c446d9e2d210ff1a7641e7d
UGUR GUNDOGDU <ugurgundogdu@me.com>
2021-04-30 02:17:44 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,5 +1,6 @@
-def parrot (Squawk!)
-  puts Squawk!
+def parrot ("Squawk!")
+  puts parrot
+  return "Squawk!"
 end# Create method `parrot` that outputs a given phrase and
 # returns the phrase
-Squawk!
+"Squawk!"
