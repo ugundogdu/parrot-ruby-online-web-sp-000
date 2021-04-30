ac8017d88497b57223f8fe6bed5cec654b08e7fd
UGUR GUNDOGDU <ugurgundogdu@me.com>
2021-04-30 03:59:40 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,5 +1,6 @@
 def parrot("Squawk!")
-  puts 
+  puts "Squawk!"
 end
-# Create method `parrot` that outputs a given phrase and
+
+parrot# Create method `parrot` that outputs a given phrase and
 # returns the phrase
